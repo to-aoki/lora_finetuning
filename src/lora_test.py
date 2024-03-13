7f7aa1f9fc9d56039cd2894c3abba80912872444
taoki <toshihiko.aoki@gmail.com>
2024-03-14 00:22:00 +0900

    load base model fix

--- src/lora_test.py
+++ src/lora_test.py
@@ -6,15 +6,15 @@ from transformers import (
     HfArgumentParser,
     AutoTokenizer,
     AutoModelForCausalLM,
-    AutoConfig
+    BitsAndBytesConfig
 )
-from peft import AutoPeftModelForCausalLM
+from peft import PeftModel, PeftConfig
 from template import templates_lookup
 
 
 @dataclass
 class ScriptArguments:
-    max_seq_length: Optional[int] = field(default=300)
+    max_seq_length: Optional[int] = field(default=512)
     use_nai_tokenizer: Optional[bool] = field(
         default=False,
     )
@@ -60,7 +60,18 @@ class ScriptArguments:
     use_unsloth:str = field(
         default=False
     )
-
+    use_nested_quant: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Activate nested quantization for 4bit base models"},
+    )
+    bnb_4bit_compute_dtype: Optional[str] = field(
+        default="bfloat16",
+        metadata={"help": "Compute dtype for 4bit base models"},
+    )
+    bnb_4bit_quant_type: Optional[str] = field(
+        default="nf4",
+        metadata={"help": "Quantization type fp4 or nf4"},
+    )
 
 parser = HfArgumentParser(ScriptArguments)
 script_args = parser.parse_args_into_dataclasses()[0]
@@ -69,12 +80,13 @@ instruct_template = templates_lookup.get(script_args.prompt_format)
 if instruct_template is None:
     raise ValueError("not found prompt_format :", script_args.prompt_format)
 
-# require flash-attn or torch 2.1 later
+# require flash-attn or torch 2. later
 attn_impl = "eager"
 if script_args.use_sdpa:
     attn_impl = "sdpa"
 if script_args.use_flash_attention_2:
     attn_impl = "flash_attention_2"
+
 
 tokenizer = None
 if script_args.base_model:
@@ -89,31 +101,67 @@ if script_args.base_model:
         )
         FastLanguageModel.for_inference(model)
     else:
+        bnb_config = None
+        if script_args.load_in_4bit or script_args.load_in_4bit:
+            compute_dtype = getattr(torch, script_args.bnb_4bit_compute_dtype)
+            bnb_config = BitsAndBytesConfig(
+                load_in_4bit=script_args.load_in_4bit,
+                load_in_8bit=script_args.load_in_8bit,
+                bnb_4bit_quant_type=script_args.bnb_4bit_quant_type,
+                bnb_4bit_compute_dtype=compute_dtype,
+                bnb_4bit_use_double_quant=script_args.use_nested_quant,
+            )
         model = AutoModelForCausalLM.from_pretrained(
             script_args.base_model,
-            load_in_4bit=script_args.load_in_4bit,
-            load_in_8bit=script_args.load_in_8bit,
+            quantization_config=bnb_config,
             device_map="auto",
             torch_dtype=torch.bfloat16 if script_args.bf16 else torch.float16,
             attn_implementation=attn_impl,
-            trust_remote_code=True)
-
-        if not script_args.load_in_4bit and not script_args.load_in_8bit:
-            model.cuda()
+            trust_remote_code=True
+        )
 
     script_args.lora_model = script_args.base_model
 else:
-    model = AutoPeftModelForCausalLM.from_pretrained(
-        script_args.lora_model,
-        load_in_4bit=script_args.load_in_4bit,
-        load_in_8bit=script_args.load_in_8bit,
-        is_trainable=False,
-        attn_implementation=attn_impl,
-        device_map="auto",
-        torch_dtype=torch.bfloat16 if script_args.bf16 else torch.float16,
-        trust_remote_code=True)
-
-    model.cuda()
+    if script_args.use_unsloth:
+        from unsloth import FastLanguageModel
+        model, tokenizer = FastLanguageModel.from_pretrained(
+            script_args.lora_model,
+            dtype=None,
+            load_in_4bit=script_args.load_in_4bit,
+            load_in_8bit=script_args.load_in_8bit,
+            # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+        )
+        FastLanguageModel.for_inference(model)
+    else:
+        bnb_config = None
+        if script_args.load_in_4bit or script_args.load_in_4bit:
+            compute_dtype = getattr(torch, script_args.bnb_4bit_compute_dtype)
+            bnb_config = BitsAndBytesConfig(
+                load_in_4bit=script_args.load_in_4bit,
+                load_in_8bit=script_args.load_in_8bit,
+                bnb_4bit_quant_type=script_args.bnb_4bit_quant_type,
+                bnb_4bit_compute_dtype=compute_dtype,
+                bnb_4bit_use_double_quant=script_args.use_nested_quant,
+            )
+        base_model_name_or_path = PeftConfig.from_pretrained(
+            script_args.lora_model).base_model_name_or_path
+        model = AutoModelForCausalLM.from_pretrained(
+            base_model_name_or_path,
+            quantization_config=bnb_config,
+            device_map="auto",
+            torch_dtype=torch.bfloat16 if script_args.bf16 else torch.float16,
+            attn_implementation=attn_impl,
+            trust_remote_code=True,
+        )
+        model = PeftModel.from_pretrained(
+            model,
+            script_args.lora_model,
+            is_trainable=False,
+            attn_implementation=attn_impl,
+            device_map="auto",
+            torch_dtype=torch.bfloat16 if script_args.bf16 else torch.float16,
+            trust_remote_code=True
+        )
 
 tokenizer_path = script_args.lora_model
 if script_args.tokenizer_model is not None:
@@ -158,7 +206,7 @@ def generate(prompt):
     outputs = model.generate(
         **inputs,
         prompt_lookup_num_tokens=10, 
-        max_new_tokens=512,
+        max_new_tokens=script_args.max_seq_length,
         do_sample=script_args.do_sample,
         top_p=0.95,
         temperature=0.2,
